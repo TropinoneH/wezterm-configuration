c41d3c54a94063227d06cab3e15def8940e79ed4
TropinoneH <mspt5871@protonmail.com>
2024-03-28 16:22:05 +0800

    init from KevinSilvester/wezterm-config

---
+++ config/bindings.lua
@@ -0,0 +1,152 @@
+local wezterm = require('wezterm')
+local platform = require('utils.platform')()
+local backdrops = require('utils.backdrops')
+local act = wezterm.action
+
+local mod = {}
+
+if platform.is_mac then
+    mod.SUPER = 'SUPER'
+    mod.SUPER_REV = 'SUPER|CTRL'
+elseif platform.is_win then
+    mod.SUPER = 'ALT'
+    mod.SUPER_REV = 'ALT|CTRL'
+elseif platform.is_linux then
+    mod.SUPER = 'ALT'
+    mod.SUPER_REV = 'ALT|CTRL'
+end
+
+local keys = {
+    -- misc/useful --
+    { key = 'F2', mods = 'NONE', action = act.ActivateCommandPalette },
+    { key = 'F3', mods = 'NONE', action = act.ShowLauncher },
+    { key = 'F4', mods = 'NONE', action = act.ShowLauncherArgs({ flags = 'FUZZY|TABS' }) },
+    {
+        key = 'F5',
+        mods = 'NONE',
+        action = act.ShowLauncherArgs({ flags = 'FUZZY|WORKSPACES' }),
+    },
+    -- toggle fullscreen
+    { key = 'F11', mods = 'NONE', action = act.ToggleFullScreen },
+    { key = 'F12', mods = 'NONE', action = act.ShowDebugOverlay },
+    { key = 'f', mods = mod.SUPER, action = act.Search({ CaseInSensitiveString = '' }) },
+    {
+        key = 'u',
+        mods = mod.SUPER,
+        action = wezterm.action.QuickSelectArgs({
+            label = 'open url',
+            patterns = {
+                'https?://\\S+',
+            },
+            action = wezterm.action_callback(function(window, pane)
+                local url = window:get_selection_text_for_pane(pane)
+                wezterm.log_info('opening: ' .. url)
+                wezterm.open_with(url)
+            end),
+        }),
+    },
+
+    -- copy/paste --
+    { key = 'c', mods = 'CTRL|SHIFT', action = act.CopyTo('Clipboard') },
+    { key = 'v', mods = 'CTRL|SHIFT', action = act.PasteFrom('Clipboard') },
+
+    -- tabs --
+    -- tabs: spawn+close
+    { key = 't', mods = mod.SUPER, action = act.SpawnTab('DefaultDomain') },
+    { key = 'q', mods = mod.SUPER, action = act.CloseCurrentTab({ confirm = false }) },
+
+    -- tabs: navigation
+    { key = '[', mods = mod.SUPER, action = act.ActivateTabRelative(-1) },
+    { key = ']', mods = mod.SUPER, action = act.ActivateTabRelative(1) },
+    { key = '[', mods = 'CTRL|SHIFT', action = act.MoveTabRelative(-1) },
+    { key = ']', mods = 'CTRL|SHIFT', action = act.MoveTabRelative(1) },
+
+    -- window --
+    -- spawn windows
+    { key = 'n', mods = mod.SUPER, action = act.SpawnWindow },
+
+    -- background controls --
+    {
+        key = 'r',
+        mods = mod.SUPER,
+        action = wezterm.action_callback(function(window, _pane)
+            backdrops:random(window)
+        end),
+    },
+    {
+        key = [[,]],
+        mods = mod.SUPER,
+        action = wezterm.action_callback(function(window, _pane)
+            backdrops:cycle_back(window)
+        end),
+    },
+    {
+        key = [[.]],
+        mods = mod.SUPER,
+        action = wezterm.action_callback(function(window, _pane)
+            backdrops:cycle_forward(window)
+        end),
+    },
+
+    -- panes --
+    -- panes: split panes
+    {
+        key = [[/]],
+        mods = mod.SUPER,
+        action = act.SplitVertical({ domain = 'CurrentPaneDomain' }),
+    },
+    {
+        key = [[\]],
+        mods = mod.SUPER,
+        action = act.SplitHorizontal({ domain = 'CurrentPaneDomain' }),
+    },
+
+    -- panes: zoom+close pane
+    { key = 'Enter', mods = mod.SUPER, action = act.TogglePaneZoomState },
+    { key = 'w', mods = 'CTRL|SHIFT', action = act.CloseCurrentPane({ confirm = false }) },
+
+    -- panes: navigation
+    {
+        key = 'p',
+        mods = mod.SUPER,
+        action = act.PaneSelect({ alphabet = '1234567890', mode = 'SwapWithActiveKeepFocus' }),
+    },
+
+    -- resize panes
+    {
+        key = 'p',
+        mods = mod.SUPER_REV,
+        action = act.ActivateKeyTable({
+            name = 'resize_pane',
+            one_shot = false,
+            timemout_miliseconds = 1000,
+        }),
+    },
+}
+
+local key_tables = {
+    resize_pane = {
+        { key = 'UpArrow', action = act.AdjustPaneSize({ 'Up', 1 }) },
+        { key = 'DownArrow', action = act.AdjustPaneSize({ 'Down', 1 }) },
+        { key = 'LeftArrow', action = act.AdjustPaneSize({ 'Left', 1 }) },
+        { key = 'RightArrow', action = act.AdjustPaneSize({ 'Right', 1 }) },
+        { key = 'Escape', action = 'PopKeyTable' },
+        { key = 'q', action = 'PopKeyTable' },
+    },
+}
+
+local mouse_bindings = {
+    -- Ctrl-click will open the link under the mouse cursor
+    {
+        event = { Up = { streak = 1, button = 'Left' } },
+        mods = 'CTRL',
+        action = act.OpenLinkAtMouseCursor,
+    },
+}
+
+return {
+    disable_default_key_bindings = true,
+    keys = keys,
+    key_tables = key_tables,
+    mouse_bindings = mouse_bindings,
+}
---
+++ config/domains.lua
@@ -0,0 +1,31 @@
+return {
+    ssh_domains = {
+        {
+            -- The connection is an ssh connection, don't use any multiplexing.
+            multiplexing = 'None',
+
+            -- The name of this specific domain. Must be unique amongst
+            name = 'lumiani',
+
+            -- identifies the host:port pair of the remote server
+            -- Can be a DNS name or an IP address with an optional
+            -- ":port" on the end.
+            remote_address = '10.19.126.149:10008',
+
+            -- Whether agent auth should be disabled.
+            -- Set to true to disable it.
+            -- no_agent_auth = false,
+
+            -- The username to use for authenticating with the remote host
+            username = 'lumiani',
+
+            -- Specify a SSH connection authentication file
+            -- Default authentication file is "~/.ssh/id_rsa"
+            ssh_option = {
+                identityfile = '~/.ssh/id_rsa',
+            },
+        },
+    },
+    unix_domains = {},
+    wsl_domains = {},
+}
